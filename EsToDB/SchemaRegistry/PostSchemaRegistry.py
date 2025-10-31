d9de843b3c6c48621dbf17877aa0a00c205a33f2
baektothefuture98 <sun4461637@naver.com>
2025-10-31 10:42:26 +0900

    Service A

---
+++ EsToDB/SchemaRegistry/PostSchemaRegistry.py
@@ -0,0 +1,36 @@
+from schema_registry.client import SchemaRegistryClient,schema
+import os, json, pathlib
+
+def schema_registry_client(schema_url : str = None) -> SchemaRegistryClient : 
+    if schema_url is None :
+        raise EnvironmentError("SCHEMA_REGISTRY environment variable is empty")  
+    return SchemaRegistryClient(url=schema_url)
+
+def get_schema_json_path() -> pathlib.Path: 
+    schema_json = list(pathlib.Path(__file__).resolve().parent.glob("schema.json"))
+    if not schema_json : 
+        raise FileNotFoundError("schema.json file not exist")
+    return schema_json[0]
+
+def read_schema_json(schema_json_path : pathlib.Path): 
+    return schema_json_path.read_text(encoding="utf-8")
+
+def post_schema_to_registry(subject_name : str, avro_schema : str, client : SchemaRegistryClient) -> int: 
+    schema_id = client.register(subject_name, avro_schema) 
+    return schema_id
+
+def main() : 
+    schema_registry_path = os.getenv("SCHEMA_REGISTRY")
+    client = schema_registry_client(schema_registry_path) 
+
+    schema_file_path = get_schema_json_path()
+    schema_file_content = read_schema_json(schema_file_path)   
+    
+    subject_name = os.getenv("SERVICE_NAME")
+    schema_id = post_schema_to_registry(subject_name, schema_file_content, client)
+    print(schema_id)
+
+    return schema_id
+
+if __name__ == "__main__": 
+    main()
