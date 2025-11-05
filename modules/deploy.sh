1373cbe9190ecb239c9c2c69d5119f3aaf7efdc2
baektothefuture98 <sun4461637@naver.com>
2025-11-05 09:22:42 +0900

    add : seonmin_duplication_check

--- modules/deploy.sh
+++ modules/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 활성화된 서비스 목록 정의 (예: "BPE", "SERVICE2", "SERVICE3")
-services=("test_project3-003")
+services=("seonmin_duplication_check")
 
 for service in "${services[@]}"; do
     echo "$service"
